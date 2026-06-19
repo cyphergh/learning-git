2cf03c10297a31b9ccd8aad3fb277819b8cb60c9
Kusi Samuel <info.kusi.gh@gmail.com>
2026-06-19 16:51:18 +0000

    adding firt-file.ts

---
+++ first-file.ts
@@ -0,0 +1,20 @@
+const userArgs = process.argv.slice(2);
+if(userArgs.length<1) {
+       	console.error("password required");
+	process.exit(0);
+}
+export async function checkPassword(password:string):Promise<boolean>{
+	let hash:string = await Bun.password.hash("try");
+	return await Bun.password.verify(password,hash);
+}
+
+try{
+ const status:boolean = await checkPassword(userArgs[0]);
+ if(status) {
+   console.log("You got the password",status);
+   } else {
+   console.log("Password error",status);
+ }
+}catch (err) {
+ console.log(err)
+}
